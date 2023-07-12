9a6972ba35ea2d46f8fe27e906865bbd3e45bf4b
zaid1982 <hemppok.kembong@gmail.com>
2023-07-12 19:59:19 +0800

    Update Constant.php

--- api/class/Constant.php
+++ api/class/Constant.php
@@ -8,7 +8,7 @@ class Constant {
     //public static $redisHost = '127.0.0.1';
     //public static $redisPort = 6379;
     public static $isLogged = true;
-    public static $folderDebug = '../../../logs/gems/task_management/';
+    public static $folderDebug = '../../../logs/task_management/debug/';
     //public static $folderDebug = 'C:\Users\User\logs\task_management\\';
     public static $folderError = '../../../logs/task_management/error/';
     public static $url = '//localhost/task_management/api/';
